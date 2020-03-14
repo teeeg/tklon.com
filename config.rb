ad1913fea06aefb1dcb36fac5849a6d5167de76d
teeeg <tommyklon@gmail.com>
2020-03-14 15:12:01 -0700

    Update typescript build, add github edit link to layout

--- config.rb
+++ config.rb
@@ -8,14 +8,14 @@
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
+# page "/path/to/file.html", layout: :otherlayout
+
 page '/404.html', directory_index: false
 
 # ignore typescript files
 ignore '/**/*.ts'
 
 # With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
@@ -50,7 +50,7 @@ end
 # compile *.ts -> *.js
 activate :external_pipeline,
   name: :webpack,
-  command: build? ? './node_modules/webpack/bin/webpack.js --bail' : './node_modules/webpack/bin/webpack.js --watch -d',
+  command: build? ? "npm run build" : "npm run build -- --watch",
   source: ".tmp/",
   latency: 1
 
